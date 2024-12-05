0001858b93906705e394cea68a4dc03e31de1c68
Nuyoahymaaa <1666856461@qq.com>
2024-12-05 22:43:21 +0800

    mmx

--- exercise.cpp
+++ exercise.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 double len(double x1,double y1,double x2,double y2){
     return sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
-}
+}//
 
 double dis(double x1,double y1,double x2,double y2,double a1,double b1){
     if(len(x1,y1,x2,y2)==0) return sqrt((x1-a1)*(x1-a1)+(y1-b1)*(y1-b1));
